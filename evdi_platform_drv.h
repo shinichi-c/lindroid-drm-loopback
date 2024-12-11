d6b9bf8331c7899f1e4366bed68cbef232bea227
Luka Panio <lukapanio@gmail.com>
2024-12-11 17:30:58 +0100

    rebrand

--- evdi_platform_drv.h
+++ evdi_platform_drv.h
@@ -23,13 +23,13 @@
 struct device;
 struct platform_device_info;
 
-#define DRIVER_NAME   "evdi"
-#define DRIVER_DESC   "Extensible Virtual Display Interface"
-#define DRIVER_DATE   "20240920"
+#define DRIVER_NAME   "evdi-lindroid"
+#define DRIVER_DESC   "Lindroid DRM Interface"
+#define DRIVER_DATE   "NEVER"
 
 #define DRIVER_MAJOR 1
-#define DRIVER_MINOR 14
-#define DRIVER_PATCH 7
+#define DRIVER_MINOR 0
+#define DRIVER_PATCH 0
 
 void evdi_platform_remove_all_devices(struct device *device);
 unsigned int evdi_platform_device_count(struct device *device);
